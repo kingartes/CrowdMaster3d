60ece4ad761019d8efb52e7703543bb9060e7c11
Ростислав <kingartes1989@gmail.com>
2021-07-04 17:58:32 +0300

    Add explosive barrel

---
+++ Assets/Scripts/Obstacles/ExplosiveBarrel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveBarrel : MonoBehaviour, IDamageable
+{
+    [SerializeField] private float _explosiveRadius;
+    [SerializeField] private float _explosiveForce;
+
+    public bool ApplyDamage(Rigidbody rigidbody, float force)
+    {
+        Collider[] appliedObjects = Physics.OverlapSphere(transform.position, _explosiveRadius);
+        Debug.Log(appliedObjects.Length);
+        foreach(var collider in appliedObjects)
+        {
+            Rigidbody item = collider.GetComponent<Rigidbody>();
+            if (item)
+            {
+                item.AddExplosionForce(_explosiveForce, transform.position, _explosiveRadius);
+            }
+        }
+        Destroy(this.gameObject);
+        return true;
+    }
+}
